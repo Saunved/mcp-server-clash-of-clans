1e345faeca32465c9155553d38d2e45c447d7b65
Saunved <saunved@gmail.com>
2025-04-05 18:30:49 +0530

    Make server executable via npx

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// index.js
+#!/usr/bin/env node
 
 import { McpServer, ResourceTemplate } from '@modelcontextprotocol/sdk/server/mcp.js';
 import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js';
